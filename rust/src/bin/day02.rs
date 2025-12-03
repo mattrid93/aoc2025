7dfa2796e9f6f58a59430e56a831089a307cdeda
Matthew Ridley <matt@Matthews-MacBook-Pro.local>
2025-12-03 14:55:43 +0000

    Review comments

--- rust/src/bin/day02.rs
+++ rust/src/bin/day02.rs
@@ -7,15 +7,11 @@ const INPUT_FILE: &str = "../inputs/day02.txt";
 fn parse_input() -> Vec<(usize, usize)> {
     let mut result: Vec<(usize, usize)> = fs::read_to_string(INPUT_FILE)
         .expect("No input file")
-        .strip_suffix("\n")
-        .unwrap()
-        .split(",")
+        .trim_end()
+        .split(',')
         .map(|pair| {
-            let vec: Vec<usize> = pair
-                .split("-")
-                .map(|s| s.parse::<usize>().unwrap())
-                .collect();
-            (vec[0], vec[1])
+            let mut it = pair.split('-').map(|s| s.parse::<usize>().unwrap());
+            (it.next().unwrap(), it.next().unwrap())
         })
         .collect();
     result.sort();
@@ -23,16 +19,13 @@ fn parse_input() -> Vec<(usize, usize)> {
 }
 
 fn invalid_id(counter: usize, reps: usize) -> usize {
-    let mut s = counter.to_string();
-    for _ in 1..reps {
-        s.push_str(&counter.to_string());
-    }
+    let s = counter.to_string().repeat(reps);
     s.parse::<usize>().unwrap()
 }
 
 fn solve_p1(input: &[(usize, usize)]) -> usize {
-    let mut total: usize = 0;
-    let mut counter: usize = 1;
+    let mut total = 0;
+    let mut counter = 1;
     let mut id = invalid_id(counter, 2);
     for &(s, e) in input {
         while id < s {
@@ -54,12 +47,22 @@ fn is_invalid_id(id: usize) -> bool {
     if n < 2 {
         return false;
     }
-    for m in 1..(n / 2 + 1) {
-        if n % m == 0 {
-            let k = n / m;
-            if s == s.get(..m).unwrap().repeat(n / m) {
-                return true;
+    for m in 1..=n / 2 {
+        if n % m != 0 {
+            continue;
+        }
+
+        let pattern = &s[..m];
+        let mut ok = true;
+
+        for chunk_start in (0..n).step_by(m) {
+            if &s[chunk_start..chunk_start + m] != pattern {
+                ok = false;
+                break;
             }
+        }
+        if ok {
+            return true;
         }
     }
     false
@@ -68,7 +71,7 @@ fn is_invalid_id(id: usize) -> bool {
 fn solve_p2(input: &[(usize, usize)]) -> usize {
     let mut total: usize = 0;
     for &(s, e) in input {
-        for i in s..(e + 1) {
+        for i in s..=e {
             if is_invalid_id(i) {
                 total += i;
             }
